2abbe690cd68e7c45a224a1f0fb2b211700d2a43
Mac-ziis <macg286@gmail.com>
2023-09-23 10:26:53 -0700

    Add description constructor to the vendors class

--- VendorOrder/Controllers/VendorsController.cs
+++ VendorOrder/Controllers/VendorsController.cs
@@ -22,22 +22,11 @@ namespace VendorOrder.Controllers
     }
 
     [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName)
+    public ActionResult Create(string vendorName, string vendorDescription)
     {
-      Vendor newVendor = new Vendor(vendorName);
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
       return RedirectToAction("Index");
     }
-
-    // [HttpGet("/vendors/{id}")]
-    // public ActionResult Show(int id)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   Vendor selectedVendor = Vendor.Find(id);
-    //   List<Order> vendorOrders = selectedVendor.Orders;
-    //   model.Add("vendor", selectedVendor);
-    //   model.Add("orders", vendorOrders);
-    //   return View(model);
-    // }
 
     [HttpGet("/vendors/{id}")]
     public ActionResult Show(int id)
@@ -46,7 +35,7 @@ namespace VendorOrder.Controllers
 
       if (selectedVendor == null)
       {
-        return NotFound(); // Return a 404 status code.
+        return NotFound(); 
       }
 
       Dictionary<string, object> model = new Dictionary<string, object>();
